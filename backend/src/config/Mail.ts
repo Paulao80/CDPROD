6bac4aac71ef4ccabd095bc63c60fcbe2619d2f6
Paulao80 <pauloviniciusjipa@gmail.com>
2022-08-15 20:02:55 -0400

    Info: Reconfigurando projeto

--- backend/src/config/Mail.ts
+++ backend/src/config/Mail.ts
@@ -1,5 +1,5 @@
 class Config {
-    public host = 'smtp.uhserver.com';
+    public host = 'smtp.mail.yahoo.com';
     public port = 587;
     public user = `${process.env.EMAIL_SEND}`;
     public password = `${process.env.PASSWORD_EMAIL}`;
